4f5d010a23b494742485cd22ad61c05265771347
Gennie Apoluva <tikana4@yahoo.com>
2018-09-10 17:25:01 -0700

    first commit

---
+++ Session 2  - String/src/strings.java
@@ -0,0 +1,12 @@
+
+public class strings {
+	public static void main (String[] args) {
+		String helloWorld = "hello world";
+		System.out.println(helloWorld);
+		
+		//concat
+		String hello = "Hello, ";
+		String name = "Charlie!";
+		System.out.println(hello + name);
+	}
+}
---
+++ Session 3 - Types and Variables/src/TypesAndVariables.java
@@ -0,0 +1,37 @@
+
+public class TypesAndVariables {
+	public static void main(String[] args) {
+		//create a data 
+		int myVariable; // declaring a variable called myVariable that stores the type int/interger
+		myVariable = 5; // initializes myVariable to the value of 5
+		
+		System.out.println(myVariable);
+		
+		myVariable = 10;
+		System.out.println(myVariable);
+		
+		myVariable = myVariable + 5;
+		//myVariable = 10 + 5
+		System.out.println(myVariable);
+		
+		
+		int myVariable2 = 100;
+		System.out.println(myVariable2 + myVariable);
+		//100 + 15
+		
+		//fractional value
+		double myVariable3 = 342.222;
+		System.out.println(myVariable3);
+		
+		
+		int intVariable = 5;
+		System.out.println(intVariable);
+		//double conversion
+		double x = 90.22;		
+		System.out.println(x);
+		x = intVariable; 
+		System.out.println(x);
+		
+	}
+
+}
